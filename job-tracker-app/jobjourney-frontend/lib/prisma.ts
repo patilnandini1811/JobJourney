96672d816fb4a1fe06e2f41b0174d27da2eba7fa
Nandini <patilnandini1811@gmail.com>
2026-02-10 13:48:45 +0100

    mplemented secure user authentication with registration, login, JWT-based sessions, and protected routes.

--- job-tracker-app/jobjourney-frontend/lib/prisma.ts
+++ job-tracker-app/jobjourney-frontend/lib/prisma.ts
@@ -1,11 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
+const createPrismaClient = () =>
   new PrismaClient({
     log: ["query"],
-  });
+    accelerateUrl: process.env.DATABASE_URL, 
+  }).$extends(withAccelerate());
+
+type PrismaAcceleratedClient = ReturnType<typeof createPrismaClient>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaAcceleratedClient;
+};
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
